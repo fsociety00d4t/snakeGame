1ab42d96f5854c56956dca65b7c6b5af851ba447
Home <58801059+fsociety00d4t@users.noreply.github.com>
2024-10-19 16:34:18 +0300

    basic movment of player inside the gameBoard

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,8 +1,19 @@
+import React, {useRef, useEffect} from 'react';
+import Player from "./Player";
 function gameBoard() {
+
+    const parentRef = useRef<HTMLDivElement | null>(null);
+
     return(
         <>
-        <div className="board">
-            <div className="test"></div>
+        <div 
+        ref={parentRef} // Pass this ref to the child
+        className="board" style={{position:'relative', width: '100vmin',
+            height: '80vmin'}}>
+            {/* <div className="test"></div> */}
+            {/* {console.log(HTMLDivElement)} */}
+            {/* @ts-ignore */}
+            <Player parentRef={parentRef}/>
         </div>
         </>
     )
